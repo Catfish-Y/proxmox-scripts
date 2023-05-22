4b7fa844276282c5b10dbe38c4a02ae246fd19c7
Catfish-Y <35066569+Catfish-Y@users.noreply.github.com>
2023-05-22 14:11:30 +0800

    Update alpine.sh

--- lxc/nginx-proxy-manager/install/alpine.sh
+++ lxc/nginx-proxy-manager/install/alpine.sh
@@ -272,7 +272,7 @@ rc-service openresty stop &>/dev/null
 set -x
 log "Starting services"
 #sed -i 's/"pid /usr/local/openresty/nginx/logs/nginx.pid"/"# pid /usr/local/openresty/nginx/logs/nginx.pid"/g' /usr/local/openresty/nginx/conf/nginx.conf
-sed -i 's/pid /# pid /g' /usr/local/openresty/nginx/conf/nginx.conf
+#sed -i 's/pid /# pid /g' /usr/local/openresty/nginx/conf/nginx.conf
 runcmd rc-service openresty start
 runcmd rc-service npm start
 set +x
